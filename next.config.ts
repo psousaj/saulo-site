ac7854332f1a8031c6f8bfd88a1c5b847c6843b8
José Pinheiro <josepsousa2012@gmail.com>
2025-02-13 15:03:39 -0300

    dale

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -11,6 +12,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'public/assets'),
+    };
+    return config;
+  },
 };
+
 
 export default nextConfig;
