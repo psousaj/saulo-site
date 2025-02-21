7e27f939be0e142471002044d5877ced6da41df6
José Pinheiro <josepsousa2012@gmail.com>
2025-02-21 00:03:29 -0300

    atualizar link do botão de compra para redirecionar para a página correta

--- src/components/ComprarButton.tsx
+++ src/components/ComprarButton.tsx
@@ -9,7 +9,7 @@ type ComprarButtonProps = {
 export function ComprarButton({ className, textClassName }: ComprarButtonProps) {
     return (
         <Link
-            href="#"
+            href="/comprar"
             className={cn(
                 "inline-block rounded-full text-center px-6 py-2 max-w-[310px] max-h-[60px] opacity-95",
                 "bg-gradient-to-bl from-[#5CCAFD] to-[#005A95] border border-[#55b6e0] shadow-[-1px_1px_40px_-5px_#49bdedb]",
